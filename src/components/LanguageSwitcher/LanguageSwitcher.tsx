bd95d0ca81ab325eb838b76f301e9b0906b60174
George Volokitin <saintgeo23@gmail.com>
2024-06-07 11:00:29 +0300

    Removed odd imports

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './language-switcher.less';
 import { useLanguage } from '../../providers/LanguageProvider';
 import type { Language } from '../../providers/LanguageProvider';
